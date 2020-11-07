b7699087bc23b38bf0fd97417e216740a14872ad
kenkono <twentynresult@yahoo.co.jp>
2020-11-07 19:16:47 +0900

    add input number

--- pos-system.py
+++ pos-system.py
@@ -14,27 +14,28 @@ class Item:
 class Order:
     def __init__(self,item_master):
         self.item_order_list=[]
+        self.item_number_list=[]
         self.item_master=item_master
     
     def add_item_order(self,item_code):
         self.item_order_list.append(item_code)
+
+    # 個数の登録
+    def add_item_number(self,item_number):
+        self.item_number_list.append(item_number)
         
     def view_item_list(self):
         for item in self.item_order_list:
             print("商品コード:{}".format(item))
+
+    # 個数の表示
+    def view_item_number(self):
+        for item in self.item_number_list:
+            print("商品個数:{}".format(item))
     
     def view_order_list(self):
         for master in self.item_master:
             print("商品名:{} 価格:{}".format(master.item_name,master.price))
-    
-    # CSVからマスター読み込み
-    # def read_master_item(self, csv):
-    #     with open(csv) as f:
-    #         item_master=[]
-    #         reader=csv.reader(f)
-    #         for row in reader:
-    #             item_master.append(Item("{}".format(row[0]),"{}".format(row[1]),"{}".format(row[2])))
-    #         return item_master
 
 class Master:
     def read_master_item(self, csv_file):
@@ -59,9 +60,14 @@ def main():
     order=Order(item_master)
     order_number=input("商品コードを入力してください。")
     order.add_item_order(order_number)
+    # 個数入力
+    item_number=input("商品個数を入力してください。")
+    order.add_item_number(item_number)
     
     # オーダー表示
     order.view_item_list()
+    # 個数表示
+    order.view_item_number()
     # 商品名と価格の表示
     order.view_order_list()
     
